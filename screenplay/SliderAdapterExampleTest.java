a695dee8c751517603647ead2dfb262a5c44a746
ShravyaPatkar <59680177+ShravyaPatkar@users.noreply.github.com>
2021-06-21 18:00:15 +0530

    Add files via upload

---
+++ screenplay/SliderAdapterExampleTest.java
@@ -0,0 +1,7 @@
+package com.example.screenplay;
+
+import static org.junit.Assert.*;
+
+public class SliderAdapterExampleTest {
+
+}
---
+++ screenplay/seat/seatTest.java
@@ -0,0 +1,7 @@
+package com.example.screenplay.seat;
+
+import static org.junit.Assert.*;
+
+public class seatTest {
+
+}
